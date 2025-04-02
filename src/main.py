118b15f31ec94e7c88aec4c2fc03c2d3b035d23a
gruby_n0fin <93051703+n0f1ndv@users.noreply.github.com>
2025-04-02 10:29:06 +0200

    print_tree

--- src/main.py
+++ src/main.py
@@ -1,21 +1,18 @@
 from menu import menu
-
+from bst import *
 
 def main():
-    # tree = BST()
+    tree = BST()
 
     # TODO: create a function to create tree
-    # tree.insert(20)
-    # tree.insert(14)
-    # tree.insert(3)
-    # tree.insert(11)
-    # tree.insert(27)
-    # tree.insert(8)
+    tree.insert(20)
+    tree.insert(14)
+    tree.insert(3)
+    tree.insert(11)
+    tree.insert(27)
+    tree.insert(8)
 
-    # tree.preorder(tree.root)
-    # print()
-
-    menu()
+    menu(tree)
 
 if __name__ == "__main__":
     main()
--- src/menu.py
+++ src/menu.py
@@ -1,33 +1,42 @@
-from bst import *
-
 def help_message():
-    print("Help Show this menu",
-    "Print      Print the tree using In-order, Pre-order, Post-order",
-    "Remove     Remove elements from the tree",
-    "Delete     Delete whole tree",
-    "Export     Export the tree to a file", # Maybe I will do it I dunno
-    "Rebalance  Rebalance the tree",
-    "Exit       Exits the program (same as CTRL+D)", sep='\n')
+    print('Help Show this menu',
+    'Print      Print the tree using In-order, Pre-order, Post-order',
+    'Remove     Remove elements from the tree',
+    'Delete     Delete whole tree',
+    'Export     Export the tree to a file', # Maybe I will do it I dunno
+    'Rebalance  Rebalance the tree',
+    'Exit       Exits the program (same as CTRL+D)', sep='\n')
 
-def menu():
-    state = ""
+def print_tree(tree):
+    print('In-order:', end=' ')
+    tree.inorder(tree.root)
+    print()
+    print('Pre-order:', end=' ')
+    tree.preorder(tree.root)
+    print()
+    print('Post-order:', end=' ')
+    tree.postorder(tree.root)
+    print()
+
+def menu(tree):
+    state = ''
     arr = []
 
-    print(f"nodes> {len(arr)}")
-    print(f"insert> {arr}")
+    print(f'nodes> {len(arr)}')
+    print(f'insert> {arr}')
 
-    while True and state != "exit":
-        state = input("action> ")
+    while True and state != 'exit':
+        state = input('action> ')
 
-        if state.lower() == "help":
+        if state.lower() == 'help':
             help_message()
-        elif state.lower() == "print":
+        elif state.lower() == 'print':
+            print_tree(tree)
+        elif state.lower() == 'remove':
             pass
-        elif state.lower() == "remove":
+        elif state.lower() == 'export':
             pass
-        elif state.lower() == "export":
+        elif state.lower() == 'rebalance':
             pass
-        elif state.lower() == "rebalance":
-            pass
-        elif state.lower() == "exit":
-            state = "exit"
+        elif state.lower() == 'exit':
+            state = 'exit'
